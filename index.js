1215813c06509c31075b73332ac3d156cffddcb4
gmorklla <gmorklla@gmail.com>
2018-10-07 15:34:04 -0500

    Using node-sense-hat module

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = express();
+const matrix = require('node-sense-hat').Leds;
 
 app.get('/', function(req, res) {
+	setMatrix();
 	res.send('Hello World!');
 });
 
 app.listen(3000, function() {
 	console.log('Example app listening on port 3000!');
 });
+
+const setMatrix = () => {
+	const O = [0, 0, 0];
+	const X = [255, 0, 0];
+
+	const cross = [
+		X, O, O, O, O, O, O, X,
+		O, X, O, O, O, O, X, O,
+		O, O, X, O, O, X, O, O,
+		O, O, O, X, X, O, O, O,
+		O, O, O, X, X, O, O, O,
+		O, O, X, O, O, X, O, O,
+		O, X, O, O, O, O, X, O,
+		X, O, O, O, O, O, O, X,
+	];
+
+	matrix.setPixels(cross);
+
+	// To fill with a single color instead
+	matrix.clear([127, 0, 0]);
+} 
